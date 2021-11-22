2d710185d2ead424077e3aaa21ad4e81cf7473eb
Sean-OConnor-VA <sean.oconnor@va.gov>
2021-11-22 07:55:34 -0500

    work thus far

---
+++ Section1/Section1.py
@@ -0,0 +1,9 @@
+print('Hello world')
+
+print('what is your name')
+myName = input()
+
+print('what is your age?')
+myAge=input()
+
+print(myName + ', You will be ' + str(int(myAge) + 1) + ' in a year.')
