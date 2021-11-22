00084fc8c53aade3be3ff1f07f244059c063a623
Sean O'Connor <61710416+Sean-OConnor-VA@users.noreply.github.com>
2021-11-22 08:49:24 -0500

    examples from video

---
+++ section2/swordfish.py
@@ -0,0 +1,7 @@
+password = input('Please submit your Password:')
+if password == 'swordfish':
+    print('Access Granted')
+elif password =='Swordfish':
+    print('Passwords are case sensitive')
+else:
+    print('Wrong Password')
